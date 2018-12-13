e3d2a85cc00ab522dce6225348267d1003d117bd
kano233333 <1320395666@qq.com>
2018-12-13 16:39:21 +0800

    主页

--- static/admin/js/common.js
+++ static/admin/js/common.js
@@ -387,3 +387,807 @@ function getCookie(c_name){
 
   return "";
 }
+
+
+//-------------------------------------------------
+//柱图
+function ssr1(el, data, title) {
+  layui.use(['jquery'],function(){
+    var $ = layui.jquery;
+  var myChart = echarts.init(document.querySelector(el));
+  var name = [],
+    value = [],
+    total = 0;
+  data.forEach(function (val, i) {
+    name[i] = val.name;
+    value[i] = val.value;
+    total += val.value;
+  })
+  if (!title) {
+    title = '单位(人)'
+  }
+  myChart.clear();
+
+  var aaa= ['0-17', '18-40', '41-60', '61-70', '71-80', '81-90', '91-100'];
+  option = {
+    grid:{
+      x:10,
+      y:10
+    },
+    xAxis: {
+      type: 'category',
+      data: aaa
+    },
+    yAxis: {
+      type: 'value'
+    },
+    tooltip : {
+      show:true,
+    },
+    series: [{
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'line',
+      symbol: 'triangle',
+      symbolSize: 20,
+      lineStyle: {
+        normal: {
+          color: '#e7e7e7',
+          width: 4,
+          type: 'dashed'
+        }
+      },
+      itemStyle: {
+        normal: {
+          borderWidth: 3,
+          borderColor: 'rgba(255,255,255,0.5)',
+          color: 'rgba(167,190,267,0.5)'
+        }
+      }
+    }]
+  };
+  myChart.setOption(option);
+  myChart.on('click', function (e) {
+    console.log(e)
+  });
+  //防止调用太快
+  var boor = true;
+
+  function resizes() {
+    boor = true;
+    myChart.resize();
+  }
+
+  $(window).resize(function () {
+    if (boor) {
+      boor = false;
+      setTimeout(resizes, 500);
+    }
+  })
+  })
+}
+
+//饼图
+function ssr2(el, data,color) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [];
+    data.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+    })
+    myChart.clear();
+    option = {
+      title: {
+        left: 'center',
+        top: 20,
+        textStyle: {
+          color: '#ccc'
+        }
+      },
+
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+
+      visualMap: {
+        show: false,
+        min: 100,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1]
+        }
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: [
+            {value: 335, name: '直接访问'},
+            {value: 310, name: '邮件营销'},
+            {value: 274, name: '联盟广告'},
+            {value: 235, name: '视频广告'},
+            {value: 400, name: '搜索引擎'}
+          ].sort(function (a, b) {
+            return a.value - b.value;
+          }),
+          roseType: 'radius',
+          label: {
+            normal: {
+              textStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              lineStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              },
+              smooth: 0.2,
+              length: 5,
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: color,
+              shadowBlur: 200,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          },
+
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: function (idx) {
+            return Math.random() * 200;
+          }
+        }
+      ]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e)
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+//横轴柱图
+function ssr3(el, data) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [],
+      total = 0;
+    data.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+      total += val.value;
+    })
+    myChart.clear();
+    option = {
+      legend: {
+        show: false
+      },
+      tooltip: {
+        trigger: 'item',
+        show: true,
+        formatter: function (res) {
+          var text = res.name + ": <br/>" + res.value + "(" + (res.value * 100 / total).toFixed() + "%)"
+          return text;
+        }
+      },
+      yAxis: {
+        type: 'category',
+        data: function () {
+          var datas = [],
+            color = ['#3fc1e8', '#4285f4', '#54b7ff', '#3fe8ca', '#5aa5ff', '#0095ff', '#1c74b3', '#45ff8f', '#66fdff', '#50e9c1', '#313186', '#7e62fb', '#bb7ef1', '#004a81', '#e9aff3'];
+          name.forEach(function (val, i) {
+            datas[i] = {
+              value: name[i],
+              textStyle: {
+                fontSize: 10,
+                color: color[i]
+              }
+            }
+          })
+          return datas;
+        }(),
+        axisTick: {
+          show: false,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          interval: 0,
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        }
+      },
+      xAxis: {
+        type: 'value',
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: true,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        }
+      },
+      grid: {
+        right: '20px',
+        top: '0',
+        left: '10px',
+        bottom: '15px',
+        containLabel: true
+      },
+      series: [{
+        //name: str.dataName,
+        type: 'bar',
+        data: data,
+        barMaxWidth: 20,
+        label: {
+          normal: {
+            show: true,
+            position: 'right',
+            textStyle: {
+              fontSize: 10
+            }
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: function (params) {
+              var colorList = ['#3fc1e8', '#4285f4', '#54b7ff', '#3fe8ca', '#5aa5ff', '#0095ff', '#1c74b3', '#45ff8f', '#66fdff', '#50e9c1', '#313186', '#7e62fb', '#bb7ef1', '#004a81', '#e9aff3'];
+              return colorList[params.dataIndex];
+            }
+          }
+        }
+      }]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e);
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+//折线图
+function ssr4(el, online, offline) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [],
+      value1 = [],
+      total = 0;
+    online.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+      total += val.value;
+    })
+    offline.forEach(function (val, i) {
+      value1[i] = val.value;
+    })
+    myChart.clear();
+    option = {
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['在线人数', '离线人数'],
+        x: 'right',
+        itemWidth: 18,
+        itemHeight: 12,
+        textStyle: {
+          color: '#72d9e3'
+        }
+      },
+      grid: {
+        top: '35px',
+        left: '20px',
+        right: '10px',
+        bottom: '5px',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        data: name,
+        axisTick: {
+          show: false,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          interval: 0,
+          rotate: 45,
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: "#72d9e3"
+          }
+        }
+      },
+      yAxis: {
+        axisLine: {
+          lineStyle: {
+            color: "#72d9e3"
+          }
+        },
+        type: 'value',
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: true,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        }
+      },
+      series: [
+        {
+          name: '在线人数',
+          type: 'line',
+          itemStyle: {
+            color: "#3fc1e8"
+          },
+          label: {
+            show: true,
+            color: '#72d9e3'
+          },
+          data: online
+        },
+        {
+          name: '离线人数',
+          type: 'line',
+          itemStyle: {
+            color: "#4285f4"
+          },
+          label: {
+            show: true,
+            color: '#72d9e3'
+          },
+          data: offline
+        }
+      ]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e);
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+function ssr6(el, online, offline) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [],
+      value1 = [],
+      total = 0;
+    online.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+      total += val.value;
+    })
+    offline.forEach(function (val, i) {
+      value1[i] = val.value;
+    })
+    myChart.clear();
+    option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '未处理',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '处理中',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '已处理',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: '其他',
+          type: 'line',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          areaStyle: {normal: {}},
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e);
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+//走势图
+function ssr5(el, data) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var axisData = [],
+      value = [];
+    // data.forEach(function (val, i) {
+    //   axisData[i] = val.name.substring(5);
+    //   value[i] = val.value;
+    // })
+
+    var links = value.map(function (item, i) {
+      return {
+        source: i,
+        target: i + 1
+      };
+    });
+    links.pop();
+    myChart.clear();
+    option = {
+      title: {
+        x: 'center',
+        y: '1%',
+        textStyle: {
+          fontSize: 16,
+          color: '#0ff'
+        }
+      },
+      tooltip: {},
+      xAxis: {
+        type: 'category',
+        boundaryGap: true,
+        data: axisData,
+        axisTick: {
+          show: true,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          interval: 0,
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: "#72d9e3"
+          }
+        }
+      },
+      grid: {
+        left: '15px',
+        top: '20px',
+        right: '25px',
+        bottom: '10px',
+        containLabel: true
+      },
+      yAxis: {
+        axisLine: {
+          lineStyle: {
+            color: "#72d9e3"
+          }
+        },
+        type: 'value',
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: true,
+          lineStyle: {
+            color: '#0ff'
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#0ff",
+            fontSize: 10,
+          },
+        }
+      },
+      series: [
+        {
+          type: 'graph',
+          layout: 'none',
+          coordinateSystem: 'cartesian2d',
+          symbolSize: 15,
+          label: {
+            normal: {
+              show: false
+            }
+          },
+          edgeSymbol: ['circle', 'arrow'],
+          edgeSymbolSize: [4, 10],
+          data: data,
+          links: links,
+          lineStyle: {
+            normal: {
+              color: '#0ff'
+            }
+          },
+          itemStyle: {
+            color: '#4892ff'
+          }
+        }
+      ]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e);
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+function ssr7(el, data) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [],
+      total = 0;
+    data.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+      total += val.value;
+    })
+    myChart.clear();
+    option = {
+      angleAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        z: 10
+      },
+      radiusAxis: {},
+      polar: {},
+      series: [{
+        type: 'bar',
+        data: [1, 2, 3, 4, 3, 5, 1],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a'
+      }, {
+        type: 'bar',
+        data: [2, 4, 6, 1, 3, 2, 1],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a'
+      }, {
+        type: 'bar',
+        data: [1, 2, 3, 4, 1, 2, 5],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a'
+      }]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e);
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
+
+function ssr8(el, data, title) {
+  layui.use(['jquery'],function() {
+    var $ = layui.jquery;
+    var myChart = echarts.init(document.querySelector(el));
+    var name = [],
+      value = [],
+      total = 0;
+    data.forEach(function (val, i) {
+      name[i] = val.name;
+      value[i] = val.value;
+      total += val.value;
+    })
+    if (!title) {
+      title = '单位(人)'
+    }
+    myChart.clear();
+
+    option = {
+      xAxis: {},
+      yAxis: {},
+      grid: {
+        x: 20,
+        y: 30,
+        x2: 20,
+        y2: 30,
+      },
+      series: [{
+        symbolSize: 20,
+        data: [
+          [10.0, 8.04],
+          [8.0, 6.95],
+          [13.0, 7.58],
+          [9.0, 8.81],
+          [11.0, 8.33],
+          [14.0, 9.96],
+          [6.0, 7.24],
+          [4.0, 4.26],
+          [12.0, 10.84],
+          [7.0, 4.82],
+          [5.0, 5.68]
+        ],
+        type: 'scatter'
+      }]
+    };
+    myChart.setOption(option);
+    myChart.on('click', function (e) {
+      console.log(e)
+    });
+    //防止调用太快
+    var boor = true;
+
+    function resizes() {
+      boor = true;
+      myChart.resize();
+    }
+
+    $(window).resize(function () {
+      if (boor) {
+        boor = false;
+        setTimeout(resizes, 500);
+      }
+    })
+  })
+}
